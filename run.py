fcf8ead9679a9c6e67bcdd77a07e35f1878d54af
JPT96 <joshuathomas96@msn.com>
2021-10-11 17:46:48 +0000

    create a def called show with self in the brackets

--- run.py
+++ run.py
@@ -8,5 +8,7 @@ class card:
         self.value = ['A', '2', '3', '4', '5', '6',  
                        '7', '8', '9', '10', 'J', 'Q', 'K'][value-1]
         self.suit = '♥♦♣♠' [suit]
-        
+    
+    def show (self):
+          
 
