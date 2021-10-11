085dc07e0e6fdfd4bb9f0ef2a0273d8a6ff71e01
JPT96 <joshuathomas96@msn.com>
2021-10-11 17:34:01 +0000

    assign the value(numbers) to the cards

--- run.py
+++ run.py
@@ -4,3 +4,5 @@ import random
 class card:
     def __init__(self, value, suit):
         self.price = value
+        self.value = ['A', '2', '3', '4', '5', '6','7', '8', '9', '10', 'J', 'Q', 'K'][value-1]
+        
